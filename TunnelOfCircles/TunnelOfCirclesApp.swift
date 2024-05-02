f98440bc3d23a2d1734b482cdec6bd38f125137a
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-02 11:17:59 -0300

    Initial Commit

---
+++ TunnelOfCircles/TunnelOfCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunnelOfCirclesApp.swift
+//  TunnelOfCircles
+//
+//  Created by Christian Manzaraz on 02/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TunnelOfCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

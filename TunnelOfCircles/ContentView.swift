aed46a8313b3cd405560edfba0089fe29e560675
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-02 12:43:18 -0300

    Section 5 Add concentric circles to your app To begin exploring the depth of your window, add a stack of shapes using a ZStack.

--- TunnelOfCircles/ContentView.swift
+++ TunnelOfCircles/ContentView.swift
@@ -11,8 +11,21 @@ import SwiftUI
 struct ContentView: View {
     @State private var colors: [Color] = [ .cyan, .blue ]
     
+    let mindiameter = 50.0
+    let diameterChange = 70.0
+    
     var body: some View {
         VStack {
+            VStack {
+                ZStack {
+                    ForEach(0..<4) { index in
+                        Circle()
+                            .stroke(lineWidth: 30)
+                            .foregroundStyle(colors[index % 2])
+                            .frame(height: mindiameter + diameterChange * Double(index))
+                    }
+                }
+            }
             Grid {
                 GridRow {
                     Text("Colors")
